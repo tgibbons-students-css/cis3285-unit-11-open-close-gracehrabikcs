a61a74f57279bd3ae834e57822e1a9c573bccab9
Grace Hrabik <ghrabik@css.edu>
2024-11-14 14:11:37 -0600

    For Unit 11 Assignment. Movedmthe LogMessage() method from the TradeProcessor class into the subclasses, TradeProcessorVersion1 and TradeProcessorVersion2 Moved the ProcessTrades()  method from the TradeProcessor class into the subclasses, TradeProcessorVersion1 and TradeProcessorVersion2 Changed the ProcessTrades class into an interface and renamed it IProcessTrades. Removed all the methods except ProcessTrades() from the interface. Removed the different "virtual" and "abstract" and "override" descriptors so that the code all compiles.

--- AbstractTrader/TradeProcessor.cs
+++ AbstractTrader/TradeProcessor.cs
@@ -8,35 +8,46 @@ using System.Threading.Tasks;
 
 namespace AbstractTrader
 {
-    public abstract class TradeProcessor
+
+    public interface ITradeProcessor
     {
-        protected abstract IEnumerable<string> ReadTradeData(Stream stream);
+        void ProcessTrades(Stream stream);
+    }
 
-        protected abstract IEnumerable<TradeRecord> ParseTrades(IEnumerable<string> tradeData);
 
+    //public abstract class TradeProcessor
+    //{
+    //protected abstract IEnumerable<string> ReadTradeData(Stream stream);
 
-        protected void LogMessage(string message, params object[] args)
+    //protected abstract IEnumerable<TradeRecord> ParseTrades(IEnumerable<string> tradeData);
+
+    /*
+    protected void LogMessage(string message, params object[] args)
+    {
+        Console.WriteLine(message, args);
+        // added for Request 408
+        using (StreamWriter logfile = File.AppendText("log.xml"))
         {
-            Console.WriteLine(message, args);
-            // added for Request 408
-            using (StreamWriter logfile = File.AppendText("log.xml"))
-            {
-                logfile.WriteLine("<log>"+message+"</log>", args);
-            }
-
+            logfile.WriteLine("<log>"+message+"</log>", args);
         }
-
-        protected abstract void StoreTrades(IEnumerable<TradeRecord> trades);
-
-        public virtual void ProcessTrades(Stream stream)
-        //public void ProcessTrades(string url)
-        {
-            var lines = ReadTradeData(stream);
-            //var lines = ReadURLTradeData(url);
-            var trades = ParseTrades(lines);
-            StoreTrades(trades);
-        }
-
 
     }
+    */
+
+    //protected abstract void StoreTrades(IEnumerable<TradeRecord> trades);
+
+    /*
+    public virtual void ProcessTrades(Stream stream)
+    //public void ProcessTrades(string url)
+    {
+        var lines = ReadTradeData(stream);
+        //var lines = ReadURLTradeData(url);
+        var trades = ParseTrades(lines);
+        StoreTrades(trades);
+    }
+    */
+
+
+    //}
+
 }
